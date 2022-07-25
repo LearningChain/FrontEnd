310cb8bb24ddffbd42fb5cb2910a4a09b55ab932
위연주 <thizzyzz@naver.com>
2022-07-26 01:03:05 +0900

    chore : 초기 개발 환경 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      Learning Chain Project Directory Setting !
+    </div>
+  );
+};
+
+export default App;
