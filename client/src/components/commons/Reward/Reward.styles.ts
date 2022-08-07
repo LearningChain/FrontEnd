2b724c618daeaaa6030fb84d9980d2cb7359e2d9
위연주 <thizzyzz@naver.com>
2022-08-07 11:30:18 +0900

    feat : Reward 컴포넌트 제작

---
+++ client/src/components/commons/Reward/Reward.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Layout = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: none;
+  width: 80px;
+  height: 80px;
+  border-radius: 50px;
+  background-color: gray;
+`;
