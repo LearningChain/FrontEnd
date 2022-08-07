f26f1fad53bf34325b18bfd08fcc4d09e71dffd1
puki4416 <puki4416@gmail.com>
2022-08-07 15:15:04 +0900

    feat : avatar 컴포넌트 및 테스트 이미지 파일 추가

---
+++ client/src/components/commons/Avatar/Avatar.styles.ts
@@ -0,0 +1,77 @@
+import styled, {css} from 'styled-components';
+import {AvatarProps} from './Avatar';
+
+export const Layout = styled.div<AvatarProps>`
+  ${(props) =>
+    props.pattern === 'leftIconAvater' &&
+    css`
+      width: 55px;
+      height: 55px;
+      position: relative;
+      img {
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 1px solid #eaeaea;
+      }
+    `}
+  ${(props) =>
+    props.pattern === 'noneIconAvater' &&
+    css`
+      width: 96px;
+      height: 96px;
+      img {
+        width: 96px;
+        height: 96px;
+        border-radius: 50%;
+        border: 1px solid #eaeaea;
+      }
+    `}
+    ${(props) =>
+    props.pattern === 'rightIconAvater' &&
+    css`
+      width: 100px;
+      height: 100px;
+      position: relative;
+      img {
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        width: 96px;
+        height: 96px;
+        border-radius: 50%;
+        border: 1px solid #eaeaea;
+      }
+    `};
+`;
+
+export const Icon = styled.div<{pattern: string}>`
+  ${(props) =>
+    props.pattern === 'leftIconAvater' &&
+    css`
+      position: absolute;
+      right: 40px;
+      width: 15px;
+      height: 15px;
+      border-radius: 50%;
+      background: #b1b1b1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
+
+  ${(props) =>
+    props.pattern === 'rightIconAvater' &&
+    css`
+      position: absolute;
+      left: 66px;
+      width: 45px;
+      height: 45px;
+      border-radius: 50%;
+      background: #b1b1b1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    `}
+`;
---
+++ client/src/components/commons/Avatar/Avatar.tsx
@@ -0,0 +1,36 @@
+import {Icon, Layout} from './Avatar.styles';
+import React from 'react';
+import testNoneuser from '../../../imgs/testImgs/user.png';
+import testUser from '../../../imgs/testImgs/test.jpg';
+
+export interface AvatarProps {
+  /* 버튼 안의 내용 */
+  children?: React.ReactNode;
+  /* 버튼 유형 결정 */
+  pattern: string;
+  /* 이미지 사용시 링크 */
+  useImg?: string;
+  /* icon 사용시 svg */
+  icon?: React.ReactNode;
+}
+
+const Avatar = ({children, ...props}: AvatarProps) => {
+  return (
+    <Layout {...props}>
+      {props.useImg === undefined ? (
+        <img src={testNoneuser} />
+      ) : (
+        <img src={testUser} />
+      )}
+      {props.pattern !== 'noneIconAvater' ? (
+        <Icon pattern={props.pattern}>{props.icon}</Icon>
+      ) : null}
+    </Layout>
+  );
+};
+
+Avatar.defaultProps = {
+  size: 'medium',
+};
+
+export default Avatar;
